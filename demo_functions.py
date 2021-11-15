950c685fe2dcd8184ade439da5810a63cee3fb8d
Luke Cullen <51770393+luke-scot@users.noreply.github.com>
2021-11-15 13:33:37 +0000

    Part 1 of intro

---
+++ demo_functions.py
@@ -0,0 +1,19 @@
+""" This file contains functions for the python_intro demo
+created for Reffficiency in 2021. Contact lshc3@cam.ac.uk for help.
+"""
+
+def cost_print(qty,price):
+  """This function multiplies the quantity of items
+  by the unit prices and prints total cost.
+
+  Inputs
+  qty - item quantity
+  price - unit price
+
+  Outputs
+  Total cost print to screen
+  """
+  # Calculation
+  cost = qty*price
+  # Output
+  print("Total cost: £{:.2f}".format(cost_total)) 
